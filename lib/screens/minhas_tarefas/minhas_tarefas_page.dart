69d92c373f55309a95a4a9ee51bbbd4a56ac0012
Fernando Maia <fernando.maia@versatecnologia.com.br>
2023-12-23 17:04:26 -0300

    Customização da listagem de tarefas

--- lib/screens/minhas_tarefas/minhas_tarefas_page.dart
+++ lib/screens/minhas_tarefas/minhas_tarefas_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class MinhasTarefasPage extends StatefulWidget {
   const MinhasTarefasPage({super.key});
@@ -12,9 +13,46 @@ class _MinhasTarefasPageState extends State<MinhasTarefasPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Minhas Tarefas'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          const SizedBox(width: 8.0),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.blueAccent,
+        onPressed: () {},
+        isExtended: false,
+        icon: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        label: const Text(
+          'Adicionar tarefa',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: ListView.separated(
+        separatorBuilder: (_, index) => const Divider(),
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int index) => ListTile(
+          leading: Image.network('https://source.unsplash.com/200x200/?tasks'),
+          title: Text("Minha tarefa ${index + 1}"),
+          subtitle: Text(
+              "Data: ${DateFormat('dd/MM/yyyy HH:mm:ss').format(DateTime.now())}"),
+          trailing: IconButton(
+            icon: const Icon(Icons.more_horiz),
+            onPressed: () {},
+          ),
+        ),
+      ),
     );
   }
 }
